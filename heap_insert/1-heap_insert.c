9b598ea90b5df219731a11f471e28e4a11b12549
Kayme976 <7508@holbertonstudents.com>
2025-04-27 13:29:37 +0200

    correction

--- heap_insert/1-heap_insert.c
+++ heap_insert/1-heap_insert.c
@@ -1,192 +1,86 @@
 #include "binary_trees.h"
 
 size_t binary_tree_size(const heap_t *tree);
-void position_inserted_node(heap_t *inserted, heap_t **root);
-void determine_insertion_path(size_t size, my_stack_t **new_node);
-void insert_new_node(heap_t *root, heap_t *new_node, my_stack_t *directions);
+void bubble_up(heap_t *node, heap_t **root);
+void insert_node(heap_t *root, heap_t *new_node, size_t path);
 
 /**
- * heap_insert - inserts node into a max binary heap
- *
- * @root: Double pointer to root of heap
- * @value: Value to assign to inserted node
- *
- * Return: Pointer to inserted node, or NULL on Fail
-*/
+ * heap_insert - inserts a value into a Max Binary Heap
+ * @root: double pointer to root node
+ * @value: value to insert
+ * Return: pointer to inserted node, or NULL
+ */
 heap_t *heap_insert(heap_t **root, int value)
 {
-	heap_t *new_node = NULL;
-	size_t tree_size = 0;
-	my_stack_t *directions = NULL;
+    heap_t *new_node;
+    size_t size;
 
-	if (root == NULL)
-	{
-		return (NULL);
-	}
+    if (!root)
+        return (NULL);
 
-	/* If pointer to root is NULL, init root with new node */
-	if (*root == NULL)
-	{
-		new_node = binary_tree_node(*root, value);
-		*root = new_node;
-		return (new_node);
-	}
+    if (!*root)
+        return (*root = binary_tree_node(NULL, value));
 
-	/* Recurse to find parent for new leaf */
-	/* First, determine size of tree */
-	tree_size = binary_tree_size(*root);
+    size = binary_tree_size(*root) + 1;
+    new_node = binary_tree_node(NULL, value);
+    if (!new_node)
+        return (NULL);
 
-	/* Second, find path to next available position */
-	determine_insertion_path(tree_size + 1, &directions);
+    insert_node(*root, new_node, size >> 1);
+    bubble_up(new_node, root);
 
-	/* Create parentless new node for insertion */
-	new_node = binary_tree_node(NULL, value);
-
-	/* Insert new node at next available position in heap */
-	insert_new_node(*root, new_node, directions);
-
-	/* Bubble up to proper position */
-	position_inserted_node(new_node, root);
-
-	/* Return pointer to inserted node */
-	return (new_node);
+    return (new_node);
 }
 
 /**
- * position_inserted_node - bubble-up inserted node to sorted position
- * @inserted: Newly inserted node
- * @root: Root of heap
+ * insert_node - insert node based on binary path
+ * @root: root of heap
+ * @new_node: node to insert
+ * @path: binary path to follow (ignores highest bit)
  */
-void position_inserted_node(heap_t *inserted, heap_t **root)
+void insert_node(heap_t *root, heap_t *new_node, size_t path)
 {
-	heap_t *temp = malloc(sizeof(heap_t));
+    while (path > 1)
+    {
+        root = (path & 1) ? root->right : root->left;
+        path >>= 1;
+    }
 
-	/* If inserted node is greater than parent, swap positions */
-	while ((inserted->parent != NULL) && (inserted->n > inserted->parent->n))
-	{
-		/* Assign parent's attributes to temp variable */
-		temp->parent = inserted->parent->parent;
-		temp->left = inserted->parent->left;
-		temp->right = inserted->parent->right;
-
-		if (temp->parent != NULL)
-		{
-			if (temp->parent->left == inserted->parent)
-			{
-				temp->parent->left = inserted;
-			}
-			else
-			{
-				temp->parent->right = inserted;
-			}
-		}
-
-		inserted->parent->parent = inserted;
-		inserted->parent->left = inserted->left;
-		inserted->parent->right = inserted->right;
-
-		if (temp->left != inserted)
-		{
-			inserted->left = temp->left;
-			if (temp->left != NULL)
-				temp->left->parent = inserted;
-			inserted->right = inserted->parent;
-		}
-		else
-		{
-			inserted->right = temp->right;
-			if (temp->right != NULL)
-				temp->right->parent = inserted;
-			inserted->left = inserted->parent;
-		}
-		inserted->parent = temp->parent;
-
-		/* Check if new node is now root */
-		if (inserted->parent == NULL)
-		{
-			*root = inserted;
-		}
-	}
+    if (path & 1)
+        root->right = new_node;
+    else
+        root->left = new_node;
+    new_node->parent = root;
 }
 
 /**
- * insert_new_node - insert node at next available position
- * @root: Root of heap
- * @new_node: New node to insert
- * @directions: Turn by turn directions to next available position
- *
- * Return: pointer to inserted node; NULL on Failure
+ * bubble_up - move node up to maintain heap property
+ * @node: newly inserted node
+ * @root: pointer to heap root
  */
-void insert_new_node(heap_t *root, heap_t *new_node, my_stack_t *directions)
+void bubble_up(heap_t *node, heap_t **root)
 {
-	heap_t *runner = root;
+    int tmp;
 
-	/* Navigate to next available position */
-	while (directions->next != NULL)
-	{
-		/* Turn left on 0 */
-		if (directions->turn_direction == 0)
-		{
-			runner = runner->left;
-		}
-		/* Turn right on 1 */
-		else
-		{
-			runner = runner->right;
-		}
-		directions = directions->next;
-	}
-
-	/* Point parent to new node */
-	if (directions->turn_direction == 0)
-	{
-		runner->left = new_node;
-	}
-	else
-	{
-		runner->right = new_node;
-	}
-
-	/* Point new node back to parent */
-	new_node->parent = runner;
+    while (node->parent && node->n > node->parent->n)
+    {
+        tmp = node->n;
+        node->n = node->parent->n;
+        node->parent->n = tmp;
+        node = node->parent;
+    }
+    if (!node->parent)
+        *root = node;
 }
 
 /**
- * determine_insertion_path - determine turns to reach next available position
- * @size: Remaining size of tree
- * @root: Root of stack
- */
-void determine_insertion_path(size_t size, my_stack_t **root)
-{
-	size_t remainder = 0;
-	my_stack_t *new_node, *last_node = NULL;
-
-	while (size > 1)
-	{
-		remainder = size % 2;
-
-		/* Push new node onto stack */
-		new_node = malloc(sizeof(my_stack_t));
-		if (new_node != NULL)
-		{
-			new_node->turn_direction = remainder;
-			new_node->next = last_node;
-		}
-		size /= 2;
-		last_node = new_node;
-	}
-	*root = last_node;
-}
-
-/**
- * binary_tree_size - measure size of binary tree
- * @tree: tree to measure
- *
+ * binary_tree_size - calculates size of binary tree
+ * @tree: tree root
  * Return: size
  */
-size_t binary_tree_size(const binary_tree_t *tree)
+size_t binary_tree_size(const heap_t *tree)
 {
-	if (tree)
-		return (binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right));
-	return (0);
+    if (!tree)
+        return (0);
+    return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
 }
