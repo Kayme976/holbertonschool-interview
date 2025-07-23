54e69942cec181d673c0eebbb1a535f090f1d510
Kayme976 <7508@holbertonstudents.com>
2025-07-23 16:24:03 +0200

    correction

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,41 +1,22 @@
 #!/usr/bin/node
-
 const request = require('request');
+const movieID = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-function fetchCharacter (url) {
-  return new Promise((resolve, reject) => {
-    request(url, (error, response, body) => {
-      if (error) return reject(error);
-      resolve(JSON.parse(body).name);
+request(url, async (error, response, body) => {
+  if (error) { console.log(error); }
+  const result = JSON.parse(body);
+
+  for (const charURL of result.characters) {
+    await new Promise((resolve, reject) => {
+      request(charURL, (err, r, body) => {
+        if (err) {
+          reject(err);
+        } else {
+          console.log(JSON.parse(body).name);
+          resolve();
+        }
+      });
     });
-  });
-}
-
-async function getCharacterNames (movieId) {
-  const movieUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
-
-  request(movieUrl, async (error, response, body) => {
-    if (error) {
-      console.error(error);
-      return;
-    }
-
-    const movieData = JSON.parse(body);
-    const characterUrls = movieData.characters;
-
-    // Fetch characters in sequence to maintain order
-    for (const url of characterUrls) {
-      try {
-        const name = await fetchCharacter(url);
-        console.log(name);
-      } catch (err) {
-        console.error(err);
-      }
-    }
-  });
-}
-
-// Get the Movie ID from the command line argument
-const movieId = process.argv[2];
-
-getCharacterNames(movieId);
+  }
+});
